75e1ebe4b5b71593f9ed09ca4a9c25971312e96d
robomex <atmorales@gmail.com>
2014-03-04 01:15:44 -0600

    housekeeping

--- instagross.js
+++ instagross.js
@@ -1,18 +1,11 @@
 if (Meteor.isClient) {
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-Template.myMap.created = function() {
-  Template.myMap.rendered = _.once(function() {
-    var mapa = L.mapbox.map('map', 'robomex.he6o03jb');  
+  
+  Template.myMap.created = function() {
+    Template.myMap.rendered = _.once(function() {
+      var mapa = L.mapbox.map('map', 'robomex.he6o03jb');  
 
-  });
-};
-
+    });
+  };
 }
 
 if (Meteor.isServer) {
